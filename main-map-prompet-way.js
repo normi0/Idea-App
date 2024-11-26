9ed891df38e43ffaca8d187087a4ecb7ff014520
achi seif <root@DESKTOP-37GK2LD.>
2024-11-27 00:50:01 +0100

    i did finished my work on adding updating deleting the divs here

--- main-map-prompet-way.js
+++ main-map-prompet-way.js
@@ -5,19 +5,26 @@ let ideas = [];
 const view1 = document.querySelector("#view1");
 const view2 = document.querySelector("#view2");
 
-const creatDivsFromIdeas = (parent,ideas) =>{
+const creatDivsFromIdeas = (parent,ideasToShow) =>{
     if (!parent) {
         console.error("parent ellement not found");
         return;
     }
+    parent.innerHTML = ""; // Clears the existing content in the parent container
 
-    ideas.forEach(({title,content,isPrivate}) => {
+    ideasToShow.forEach((idea,index) => {
+        const {title,content,isPrivate} = idea;
         const newDiv = document.createElement("div");
         newDiv.classList.add(isPrivate? "created-div-private":"created-div");
         const divTitle = document.createElement("h2");
         divTitle.textContent = title;
         const divParagraph = document.createElement("p");
         divParagraph.textContent = content;
+
+        newDiv.addEventListener("click",()=>{
+            updateIdea(index);
+        })
+
         newDiv.append(divTitle,divParagraph);
         parent.append(newDiv);
     });
@@ -31,7 +38,7 @@ const addIdea = () =>{
         alert("PLease enter bouth title and the content of your idea !!");
         return;
     }
-    if (isPrivateInpute !== "yes" && isPrivate !== "no") {
+    if (isPrivateInpute !== "yes" && isPrivateInpute !== "no") {
         alert ("Please specify yes or no for privacy");
         return;
     }
@@ -40,6 +47,7 @@ const addIdea = () =>{
     ideas.push({
         title,content,isPrivate
     });
+    updateView();
 
     if (isPrivate) {
         view2.style.display ="flex";
@@ -49,11 +57,42 @@ const addIdea = () =>{
        creatDivsFromIdeas(view1,ideas.filter((idea)=> !idea.isPrivate));
     }
     const addAnother = prompt("Do you want to add another idea? (yes/no)").trim().toLocaleLowerCase();
-    if (addAnother == "yes") {
+    if (addAnother === "yes") {
         addIdea();
     } else {
         alert("thanks for your Ideas!!");
     }
+};
+const updateIdea = (index)=>{
+    const idea = ideas[index];
+
+    if (!idea) {
+        alert("idea not found!");
+        return;
+    }
+    const newTitle = prompt("enter the new title for your idea");
+    const newContent  = prompt("enter the new content for your idea");
+    const isPrivateInpute = prompt("is your idea private or no (yes/no)?",idea.isPrivate?"yes":"no").trim().toLowerCase();
+    if (newTitle ===""|| newContent === "") {
+        alert("Title or content cannot be empty!");
+        return;
+    }
+    if (isPrivateInpute !== "yes" && isPrivateInpute !="no") {
+        alert("idea must be private or public specify yes or no for privacy");
+        return;
+    }
+
+    ideas[index] = {
+        title:newTitle,
+        content:newContent,
+        isPrivate:isPrivateInpute ==="yes",
+    };
+   
+    updateView();
+};
+const updateView = ()=>{ 
+    creatDivsFromIdeas(view1,ideas.filter((idea)=> !idea.isPrivate));
+    creatDivsFromIdeas(view2,ideas.filter((idea)=> idea.isPrivate));
 };
 addIdea();
 });
